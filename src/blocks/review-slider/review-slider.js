593d8162989f1bc58329757ee195dd45a2187fe9
Igor <des.yogi@gmail.com>
2025-04-17 19:03:06 +0300

    index preview

---
+++ src/blocks/review-slider/review-slider.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const slides = document.querySelectorAll('.review-slider__container .swiper-slide');
+  const totalSlides = slides.length;
+  const middleSlideIndex = Math.floor(totalSlides / 2);
+
+  const review = new Swiper('.review-slider__container', {
+    speed: 400,
+    spaceBetween: 16,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    initialSlide: middleSlideIndex,
+    rewind: true,
+    breakpoints: {
+      768: {
+        slidesPerView: 'auto'
+      },
+      1440: {
+        slidesPerView: 'auto',
+        spaceBetween: 32
+      }
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  //Fancybox.bind('[data-fancybox="showroom"]', {
+    // Your custom options for a specific gallery
+  //});
+});
